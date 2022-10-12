21fa10bde51f353221b2ec7ce86c13fc95743437
vallzy <45981588+vallzy@users.noreply.github.com>
2022-10-12 20:29:09 +0200

    Dijkstra implementation
    Path finding using Dijsktras shortest path. However since Dijkstras method doesn't constantly update shortest path, we can backtrack the previous state.

---
+++ algorithms/dijkstra.py
@@ -0,0 +1,32 @@
+from math import inf
+def algorithm(graph, start, end):
+    unvisited = list(graph)
+    shortest = {}
+    previous = {start: -1}
+    for v in unvisited:
+        shortest[v] = inf
+    shortest[start] = 0
+    while len(unvisited):
+        curr_min = None
+        for v in unvisited:
+            if curr_min == None or shortest[v] < shortest[curr_min]: 
+                curr_min = v
+        connections = graph[curr_min]
+
+        for connection in connections:
+            edgeval = shortest[curr_min] + 1 #assume each edge weight is equal (= 1)
+            if edgeval < shortest[connection]:
+                shortest[connection] = edgeval
+                previous[connection] = curr_min
+        unvisited.remove(curr_min)
+    return backtrack(start, end, previous)
+
+
+def backtrack(start, end, prev): 
+    path = [end]
+    tmp = end
+    while tmp != -1:
+        tmp = prev[tmp]
+        if tmp > -1:
+            path.append(tmp)
+    return path[::-1]
