5ad641e8e74d11318ec9f08ee70aae586826697a
Mate <gubee95@gmail.com>
2022-12-12 14:24:55 +0100

    Added  function to run simulation multiple times.

--- traverse_test.py
+++ traverse_test.py
@@ -1,5 +1,7 @@
 from util import graph, robustness, traverse_simulator
 from algorithms import backwards_dijkstra
+import datetime
+import json
 
 G = graph.create_unweighted_multidigraph(5,5)
 
@@ -18,6 +20,20 @@ graph.create_obstacle(17,19,1,G)
 robustness.robustness_calculation(G)
 
 G = backwards_dijkstra.algorithm(G, 0, 4, 2)
-#print(G.nodes())
 
-print(traverse_simulator.traverse(G, 24, 10))
+
+def run_simulation(number_of_iterations, graph, start, disturbance_chance):
+    result = {}
+    _now = datetime.datetime.now()
+    filename = f"{_now.day}_{_now.hour}_{_now.minute}_{_now.second}"
+
+    for current_iteration in range(number_of_iterations):
+        current_traversal = traverse_simulator.traverse(graph, start, disturbance_chance)
+        result[current_iteration] = current_traversal
+
+    with open(f"./simulation_logs/{filename}.json", "w") as file:
+        json.dump(result, file, indent=None)
+
+
+# print(traverse_simulator.traverse(G, 24, 60))
+run_simulation(10, G, 24, 60)
--- util/traverse_simulator.py
+++ util/traverse_simulator.py
@@ -1,7 +1,8 @@
 import random
 
 def traverse(graph, start, disturbance_chance):
-    state = (True, start)
+    disturbance_counter = 0
+    state = (True, start, disturbance_counter)
     current_node_index = start
 
     #print(current_node)
@@ -11,16 +12,16 @@ def traverse(graph, start, disturbance_chance):
         predicted_index = current_node['path_to_goal'][1]
         
         if current_node['is_danger']:
-            return (False, current_node_index)
+            return (False, int(current_node_index), disturbance_counter)    # Had to cast as int since it returned as a numpy element
 
         for edge in graph.edges(current_node_index, data=True):
             if edge[2]['is_disturbance']:
                 get_disturbed = random.randint(1, 100) <= disturbance_chance
                 if get_disturbed:
                     predicted_index = edge[1]
-                    break
+                    disturbance_counter += 1
         
         current_node_index = predicted_index
-        state = (True, current_node_index)
+        state = (True, current_node_index, disturbance_counter)
     return state
         
